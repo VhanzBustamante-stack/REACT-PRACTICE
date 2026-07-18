27813cf8cfc12bb837d64bf218a8a78d2ac566b9
Vhanz Bustamante <vhanzbustamante13@gmail.com>
2026-07-18 20:54:34 +0800

    react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react';
-import Navbar from './Navbar';
-import Home from './Home';
+import { useState } from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar />
-     <div className="content">
-      <Home />
-     </div>
+      <Navbar />
+      <BrowserRouter>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Create">
+              <Create />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-   <nav className="navbar">
-    <h1>The Vhanz Blog</h1>
-    <div className="links">
+    <nav className="navbar">
+      <h1>The Vhanz Blog</h1>
+      <div className="links">
         <a href="/">Home</a>
-        <a href="/create" style=
-        {{color: "white",
-         backgroundColor: "#f1356d",
-          borderRadius: "8px"}}>New blog</a>
-        <a href="/">Contact</a>
-    </div>
-   </nav>
-  )
-}
+        <a
+          href="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New blog
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
